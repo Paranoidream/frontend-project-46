f3840ea4e5ff080b7ea3faa12ea44624a884f196
Aleksandra <missing1311@gmail.com>
2022-09-12 15:08:56 +0300

    step1

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(generateDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse(process.argv);
